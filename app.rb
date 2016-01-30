26605115179ffd8a01c9b841d0ee8d9730601be9
benser1 <briganenser28@gmail.com>
2016-01-30 14:36:19 -0700

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
 	end
-end
+
+  get '/info' do
+    erb :info
+  end  ## when we go to our webpage URL/info we will see this printed
+
+
+end ## class end
