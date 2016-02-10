818ad53859a338a872f34a576f5d8d1b2a81f7ac
pacosta613 <pedro.acosta613@gmail.com>
2016-02-10 13:18:35 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		"<h1>Hello World This HTML code is inside of a '.erb' file</h1>"
 	end
+
+  get '/' do
+    erb :index
+  end
+
+  get "/info" do
+    erb :info
+  end
+
 end
