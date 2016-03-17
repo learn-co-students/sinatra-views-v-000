03a9ef0283790d63c7fbac95871dc350a7996f36
Lady Codington <myemailforcoding@gmail.com>
2016-03-17 15:55:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+  	set :views, "./views"
+  	set :public_dir, "public"
+  end
 
-	get '/' do
-		
-	end
+  get "/" do
+  	erb :index
+  end
+
+  get "/info" do
+    erb :info
+  end
 end
