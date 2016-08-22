8b5d5bd78821ac2ded03c89497cad5b2304a446c
Lenn Hypolite <lenntriniman@hotmail.com>
2016-08-22 04:54:45 +0000

    Done

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+	# Sinatra is also configured by default to look for our .erb files in a directory called views.
 	get '/' do
-		
+		erb :index  # This tells Sinatra to render a file called index.erb inside of a directory called views.
 	end
+
+	get '/info' do
+		erb :info
+	end
+
 end
