ae809d9bcd5547a95b2095533478053b8543373f
Susanna Klosko <sdklos@gmail.com>
2017-07-13 04:22:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -10,4 +10,16 @@ class App < Sinatra::Base
 	get "/info" do
 		erb :info
 	end
+
+	get "/hello" do
+		erb :hello
+	end
+
+	get "/goodbye" do
+		erb :goodbye
+	end
+
+	get "/date" do
+		erb :date
+	end
 end
