abc0f3ce2e34b14e90cdd8fd65b1fb2c4283b764
github username <github email address>
2017-10-26 08:56:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		# "<h1>Hello World</h1>" just stuffing the html within this file
+		erb :index #loads index.erb
 	end
+
+	get "/info" do
+    erb :info
+  end
+
 end
