b870b5fadd1770bff8c9fd9dc881d5fc7d9d600b
Yomaira Escano <yjescano@gmail.com>
2018-01-13 04:11:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+	# get '/' do
+	# 	"<h1>Hello World</h1>"
+	# end
+
 	get '/' do
-		
+		erb :index
 	end
+
+	get "/info" do
+		erb :info
+	end
+
 end
