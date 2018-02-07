6b9bffe389996beb11c4a63df5d706899f168e61
Autumn J <autumnkangas@gmail.com>
2018-02-06 23:57:17 -0600

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
 	end
+
+	get "/info" do
+  	erb :info #name of file doesn't have to match the name of route, but it's best for convention's sake
+  end
+
 end
