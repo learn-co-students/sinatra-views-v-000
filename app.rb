c37e8b9c931693e822e4a1ac4d4e4b1fbdb34f6f
EC2 Default User <ec2-user@ip-172-31-14-103.us-east-2.compute.internal>
2018-03-04 20:55:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -9,4 +9,16 @@ class App < Sinatra::Base
 	get "/info" do
       erb :info
     end
+    
+    get '/hello' do
+    	erb :hello
+    end
+    
+    get '/goodbye' do
+    	erb :goodbye
+    end
+    
+    get '/date' do
+    	erb :date
+    end
 end
