927be39f95dd8c4ccc6918f6d44824dd63608d4b
Dani Schuhman <dani8439@aol.com>
2018-07-10 17:32:56 +0000

    refresher

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
-	end
+		get '/' do
+			erb :index
+		end
 
-	get "/info" do
-		erb :info
-	end
+		get "/info" do
+			erb :info
+		end
 end
