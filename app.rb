106e327deceb4580088317ffd929f52b2267d4d5
Madeline Stark <madeline.stark1@gmail.com>
2018-09-08 22:59:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		
-	end
+		get '/' do
+			erb :index #tells Sinatra to render a file called index.erb
+		end
+
+
+		get "/info" do #will show up under url plus /info
+      erb :info #name of file doesn't need to match name of route, although is convention
+    end
+
+
 end
