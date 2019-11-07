dbe0d67d0e3d1a84f787484d82cfbf91d1abde2a
Brandon Brasson <rocksong622@gmail.com>
2019-11-07 23:22:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,12 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+
 	get '/' do
+			erb :index
+		end
 
-	end
-
-  get '/' do
-      "<h1>Hello World</h1>"
-  end
+	get '/info' do
+	     erb :info
+	  end
 
 end
