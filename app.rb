cb40afc4b756ab2a0ae1b5364e51eed537a600d5
Brandon Brasson <rocksong622@gmail.com>
2019-12-03 20:20:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,4 +11,37 @@ class App < Sinatra::Base
 	     erb :info
 	  end
 
+		get '/' do
+			"<h1>Hello World</h1>"
+		end
+
+		get '/' do
+			erb :index
+		end
+
+		get '/' do
+			erb :index
+		end
+
+		get "/info" do
+			"Testing the info page"
+		end
+
+		get '/' do
+			erb :index
+		end
+
+		get "/info" do
+			erb :info
+		end
+
+		get '/' do
+			erb :index
+		end
+
+		get "/info" do
+			erb :dogs
+		end
+
+
 end
